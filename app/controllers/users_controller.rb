fc7c4765c2b1b8a3d651aad32814197ef844a287
Amer <amer.jaganjac@yahoo.com>
2018-01-16 16:58:05 -0500

    Routes aangepast en my listing toegevoegd zodat de gebruiker een comand pagina heeft voor zijn stock collectie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
 
-def my_portfolio
-
-end
+ def my_portfolio
+    @user = current_user
+    @user_stocks = current_user.stocks
+ end
+  
+ def my_listing
+   @user = current_user
+   @user_stocks = current_user.stocks
+ end
 
 end
